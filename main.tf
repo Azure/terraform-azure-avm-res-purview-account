cf7ab6a45397434c0cd66b6639ada135737bbbe5
Tripuramallu Sri Harsha <tsriharsha@microsoft.com>
2026-07-23 17:48:44 +0530

    feat:Developed the module for the purview

--- main.tf
+++ main.tf
@@ -1,25 +1,96 @@
-# TODO: Replace this dummy resource azurerm_resource_group.TODO with your module resource
-resource "azurerm_resource_group" "TODO" {
-  location = var.location
-  name     = var.name # calling code must supply the name
-  tags     = var.tags
+resource "azapi_resource" "this" {
+  location  = var.location
+  name      = var.name
+  parent_id = var.parent_id
+  type      = var.resource_types.purview_account
+
+  body = {
+    properties = {
+      managedEventHubState                = var.managed_event_hub_state
+      managedResourceGroupName            = var.managed_resource_group_name
+      managedResourcesPublicNetworkAccess = var.managed_resources_public_network_access
+      publicNetworkAccess                 = var.public_network_access
+    }
+  }
+  ignore_null_property      = true
+  replace_triggers_refs     = ["body.properties.managedResourceGroupName"]
+  response_export_values    = ["*"]
+  retry                     = var.retry
+  schema_validation_enabled = false
+  tags                      = var.tags
+
+  dynamic "identity" {
+    for_each = local.managed_identities.system_assigned_user_assigned
+
+    content {
+      identity_ids = identity.value.user_assigned_resource_ids
+      type         = identity.value.type
+    }
+  }
+
+  dynamic "timeouts" {
+    for_each = var.timeouts == null ? [] : [var.timeouts]
+
+    content {
+      create = timeouts.value.create
+      delete = timeouts.value.delete
+      read   = timeouts.value.read
+      update = timeouts.value.update
+    }
+  }
 }
 
 # required AVM resources interfaces
-resource "azurerm_management_lock" "this" {
+resource "azurerm_management_lock" "lock" {
   count = var.lock != null ? 1 : 0
 
   lock_level = var.lock.kind
   name       = coalesce(var.lock.name, "lock-${var.lock.kind}")
-  scope      = azurerm_resource_group.TODO.id # TODO: Replace with your azurerm resource name
+  scope      = azapi_resource.this.id
   notes      = var.lock.kind == "CanNotDelete" ? "Cannot delete the resource or its child resources." : "Cannot delete or modify the resource or its child resources."
 }
 
-resource "azurerm_role_assignment" "this" {
+resource "azurerm_monitor_diagnostic_setting" "diagnostic_setting" {
+  for_each = var.diagnostic_settings
+
+  name                           = each.value.name != null ? each.value.name : "diag-${var.name}"
+  target_resource_id             = azapi_resource.this.id
+  eventhub_authorization_rule_id = each.value.event_hub_authorization_rule_resource_id
+  eventhub_name                  = each.value.event_hub_name
+  log_analytics_destination_type = each.value.log_analytics_destination_type
+  log_analytics_workspace_id     = each.value.workspace_resource_id
+  storage_account_id             = each.value.storage_account_resource_id
+
+  dynamic "enabled_log" {
+    for_each = each.value.log_categories
+
+    content {
+      category = enabled_log.value
+    }
+  }
+
+  dynamic "enabled_log" {
+    for_each = each.value.log_groups
+
+    content {
+      category_group = enabled_log.value
+    }
+  }
+
+  dynamic "enabled_metric" {
+    for_each = each.value.metric_categories
+
+    content {
+      category = enabled_metric.value
+    }
+  }
+}
+
+resource "azurerm_role_assignment" "role_assignment" {
   for_each = var.role_assignments
 
   principal_id                           = each.value.principal_id
-  scope                                  = azurerm_resource_group.TODO.id # TODO: Replace this dummy resource azurerm_resource_group.TODO with your module resource
+  scope                                  = azapi_resource.this.id
   condition                              = each.value.condition
   condition_version                      = each.value.condition_version
   delegated_managed_identity_resource_id = each.value.delegated_managed_identity_resource_id
